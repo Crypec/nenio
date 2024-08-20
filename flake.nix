c9b1d418290aac75c750c369c270464c018770b5
.ctx <me@ctx.dev>
2024-08-20 18:33:52 +0200

    switch to nixpkgs/master

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nenio: ctx's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/master";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
