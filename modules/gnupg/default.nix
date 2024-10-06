a14748816c5f0ce903b3d4d5a7e579155d7e62e9
.ctx <me@ctx.dev>
2024-10-06 13:51:30 +0200

    wip

---
+++ modules/gnupg/default.nix
@@ -0,0 +1,12 @@
+{...}:
+{ 
+
+  services.pcscd.enable = true;
+  programs.gnupg = {
+    agent = {
+       pinentryPackage = pkgs.pinentry-curses;
+       enable = true;
+       enableSSHSupport = true;
+    };
+  };
+}
